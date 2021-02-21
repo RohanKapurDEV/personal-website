05ffd6820811e1a846a40ba83604d53b9c5e16c1
RohanXKapur <rohankapurdev@outlook.com>
2021-02-22 04:10:02 +0530

    ...

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Image from "next/image";
+import Head from "next/head";
 
 const Bodywrapper = styled.div`
   width: 48rem;
@@ -37,37 +38,62 @@ const IntroText = styled.div`
   /* background-color: red; */
 `;
 
-// constZ
+const BuiltWrapper = styled.div`
+  margin: 3rem 1rem;
+`;
+
+const BlogWrapper = styled.div`
+  margin: 3rem 1rem;
+`;
 
 export default function Home() {
   return (
-    <Bodywrapper>
-      <Header>
-        <HeaderTitle>👋 Hi, I'm Rohan.</HeaderTitle>
-        <HeaderContactBar>
-          <Image
-            onClick={() => {
-              console.log("click");
-            }}
-            src="/images/twitter.png"
-            height="32px"
-            width="32px"
-          ></Image>
-          <div style={{ width: "1rem" }}></div>
-          <Image src="/images/gmail.png" height="32px" width="32px"></Image>
-        </HeaderContactBar>
-      </Header>
+    <>
+      <Head>
+        <title>Rohan's website</title>
+        <meta name="viewport" content="width=initial-scale=1.0, width=device-width" />
+      </Head>
+      <Bodywrapper>
+        <Header>
+          <HeaderTitle>👋 Hi, I'm Rohan.</HeaderTitle>
+          <HeaderContactBar>
+            <Image
+              onClick={() => {
+                console.log("click");
+              }}
+              src="/images/twitter.png"
+              height="32px"
+              width="32px"
+            ></Image>
+            <div style={{ width: "1rem" }}></div>
+            <Image src="/images/gmail.png" height="32px" width="32px"></Image>
+          </HeaderContactBar>
+        </Header>
 
-      <IntroRow>
-        <Image src="/images/me.png" height="150" width="150"></Image>
-        <div style={{ width: "1rem" }}></div>
-        <IntroText>
-          I’m an independent software researcher that's passionate about the decentralisation of the traditional
-          institutions that underpin society. This is why most of the software I work on is to help advance the adoption
-          of decentralised money systems. <br></br> <br></br> The easiest ways to contact me are by using any of the
-          options available in the header.
-        </IntroText>
-      </IntroRow>
-    </Bodywrapper>
+        <IntroRow>
+          <Image src="/images/me.png" height="150" width="150"></Image>
+          <div style={{ width: "1rem" }}></div>
+          <IntroText>
+            I’m an independent software researcher that's passionate about the decentralisation of the traditional
+            institutions that underpin society. This is why most of the software I work on is to help advance the
+            adoption of decentralised money systems like Bitcoin. <br></br> <br></br> The easiest ways to contact me are
+            by using any of the options available in the header.
+          </IntroText>
+        </IntroRow>
+
+        <BuiltWrapper>
+          <h1>Things I've built:</h1>
+          <p onClick={() => {}} style={{ textDecoration: "underline", cursor: "pointer" }}>
+            Paymint - A bitcoin wallet for Android devices
+          </p>
+          <p>More here soon ;)</p>
+        </BuiltWrapper>
+
+        <BlogWrapper>
+          <h1>Blog posts:</h1>
+          <p>Sorry, nothing to see here for now :(</p>
+        </BlogWrapper>
+      </Bodywrapper>
+    </>
   );
 }
